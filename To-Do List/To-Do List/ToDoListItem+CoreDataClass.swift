6fdde7fb7098ac252846531874a8ec7b2074bc42
Arman Jain <arman.jain@zopsmart.com>
2024-07-12 16:48:47 +0530

    added todo app

---
+++ To-Do List/To-Do List/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  To-Do List
+//
+//  Created by Raramuri on 12/07/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ To-Do List/To-Do List/ViewController.swift
@@ -0,0 +1,150 @@
+//
+//  ViewController.swift
+//  To-Do List
+//
+//  Created by Raramuri on 12/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var models = [ToDoListItem]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getAllItems()
+        
+        title = "To-Do List"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+    }
+    
+    
+    @objc private func didTapAdd() {
+        let alert = UIAlertController(title: "New Item", message: nil, preferredStyle: .alert)
+        alert.addTextField(configurationHandler: nil)
+        
+        let action = UIAlertAction(title: "Submit", style: .cancel, handler: { _ in
+            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else{
+                return
+            }
+
+            self.createItem(name: text)
+        })
+        
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+    func getAllItems() {
+        do {
+            models = try context.fetch(ToDoListItem.fetchRequest())
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }catch {
+            print("Error")
+        }
+    }
+    
+    func createItem(name: String){
+        let newItem = ToDoListItem(context: context)
+        
+        newItem.name = name
+        newItem.createdAt = Date()
+        
+        do {
+            try context.save()
+            getAllItems()
+        }catch {
+            print("Error")
+        }
+    }
+    
+    func deleteItem(item: ToDoListItem) {
+        context.delete(item)
+        
+        do {
+            try context.save()
+            getAllItems()
+        }catch {
+            print("Error")
+        }
+    }
+    
+    func updateItem(item: ToDoListItem, newName: String) {
+        item.name = newName
+        
+        do {
+            try context.save()
+            getAllItems()
+        }catch {
+            print("Error")
+        }
+    }
+
+
+}
+
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let item = models[indexPath.row]
+        let sheet = UIAlertController(title: "Edit", message: nil, preferredStyle: .actionSheet)
+        
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { _ in
+            
+            let alert = UIAlertController(title: "Edit Item", message: "", preferredStyle: .alert)
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            
+            let action = UIAlertAction(title: "Save", style: .cancel, handler: { _ in
+                guard let field = alert.textFields?.first, let newName = field.text, !newName.isEmpty else {
+                    return
+                }
+                self.updateItem(item: item , newName: newName )
+            })
+            
+            alert.addAction(action)
+            self.present(alert, animated: true)
+        }))
+        
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: {_ in
+            self.deleteItem(item: item)
+        }))
+        
+        present(sheet, animated: true)
+    }
+}
+
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model  = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model.name
+        return cell
+    }
+    
+    
+}
