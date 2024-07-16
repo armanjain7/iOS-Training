f6455795ba9d69a11e9c1a4cffeca0741950ad42
Arman Jain <arman.jain@zopsmart.com>
2024-07-16 13:16:55 +0530

    Initial Commit

---
+++ ToDoApp/ToDoApp/SignUpPage.swift
@@ -0,0 +1,36 @@
+//
+//  SignUpPage.swift
+//  ToDoApp
+//
+//  Created by Raramuri on 16/07/24.
+//
+
+import UIKit
+
+class SignUpPage: UIViewController {
+
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func confirmDetails(_ sender: UIButton) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Update Details/Pods/Target Support Files/Pods-Update Details-Update DetailsUITests/Pods-Update Details-Update DetailsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Update_Details_Update_DetailsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Update_Details_Update_DetailsUITests
+@end
